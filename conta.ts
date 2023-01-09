846b0024ad99130d298016adf6a0c4fe4b75f9b1
Raphael Nobre <nobre.roots@gmail.com>
2023-01-09 16:32:41 -0300

    getters e setters pessoa

--- conta.ts
+++ conta.ts
@@ -3,17 +3,24 @@ import { Sacar } from "./sacar.js"
 
 export class Conta {
     numero: string;
+    limite: number;
     credito: Array<Credito> = [];
     debito: Array<Sacar> = [];
+
+    get
     
     depositar(valor: number, data: Date) {
-        const deposito = new Credito(valor, data);
+        let deposito = new Credito(valor, data);
         this.credito.push(deposito);
     }
 
     sacar(valor: number, data: Date) {
-        const saque = new Sacar(valor, data);
-        this.debito.push(saque);
+        if (valor <= this.limite) {
+            let saque = new Sacar(valor, data);
+            this.debito.push(saque);
+        } else {
+            console.log("Saldo indisponível");
+        }
     }
     
 }
