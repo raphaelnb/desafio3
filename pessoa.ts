be7c351d7f8d44e7f4b4804e3ba1544ecafd5954
Raphael Nobre <nobre.roots@gmail.com>
2023-01-09 16:47:17 -0300

    constructor pessoa

--- pessoa.ts
+++ pessoa.ts
@@ -3,15 +3,33 @@ class Pessoa {
     private _nome: string
     private _telefone: string
 
-    public get nome(): string {
-        return this._nome;
+    constructor (cpf: string, nome: string, telefone: string) {
+        this._cpf = cpf;
+        this._nome = nome;
+        this._telefone = telefone;
     }
 
     public get cpf(): string {
         return this._cpf;
     }
 
+    public set cpf(cpf: string) {
+        this._cpf = cpf;
+    }
+
+    public get nome(): string {
+        return this._nome;
+    }
+
+    public set nome(nome: string) {
+        this._nome = nome;
+    }
+
     public get telefone(): string {
         return this._telefone;
+    }
+
+    public set telefone(telefone: string) {
+        this._telefone = telefone;
     }
 }
